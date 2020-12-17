70a5d397cd6507004db036f23db49d70c9854fb8
mcardenaslipigas <mcardenas@lipigas.cl>
2020-12-17 12:44:41 -0300

    add comments on installer

--- install.sh
+++ install.sh
@@ -193,6 +193,7 @@ sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="pygmalion"/g' ~/.zshrc
 # Add Custom config on zshrc
 if [[ $(cat ~/.zshrc | grep 'NVM_DIR') == "" ]]; then
   cat <<EOT >> ~/.zshrc
+  # Add custom NVM configs
   export NVM_DIR=~/.nvm
   source $(brew --prefix nvm)/nvm.sh
 EOT
